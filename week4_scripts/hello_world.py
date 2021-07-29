b951fefcb322695e9848cfa76e3491184bf09b81
Harry Moore <harry.moore@astrazeneca.com>
2021-07-29 13:30:59 +0100

    updated ML project, split off week 5 content into separate notebook

--- week4_scripts/hello_world.py
+++ week4_scripts/hello_world.py
@@ -1,5 +1,9 @@
-def main():
-    print('Hello, world')
+from random import randint
 
+def main():
+    int1 = randint(1,4)
+    int2 = randint(2,7)
+    # print('Hello, world')
+    print(int1 + int2)
 if __name__ == '__main__':
     main()
